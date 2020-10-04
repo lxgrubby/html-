c8b0a4dcf4e25c1c2aeaef409c26b6094046a553
lxgrubby <69564400+lxgrubby@users.noreply.github.com>
2020-10-04 11:21:33 +0800

    Add files via upload

---
+++ HTML项目 网上书城 部分/data.js
@@ -0,0 +1,44 @@
+let users = [
+    {
+        id: "1",
+        loginName: "lxgrubby",
+        cipher: "7888218a",
+        name: "grubby"
+    },
+    {
+        id: "2",
+        loginName: "yf123456",
+        cipher: "123456",
+        name: "yf"
+
+    },
+    {
+        id: "3",
+        loginName: "yanfan123456",
+        cipher: "123456789",
+        name: "lisi"
+    }
+]
+let products = [
+    {
+        id: "1",
+        pname: "请回答1988",
+        price: 24,
+        pdesc: "《请回答1988》是由申源浩执导，李有静编剧，李惠利、朴宝剑、柳俊烈、高庚杓等主演的青春怀旧剧",
+        imgUrl: "img/请回答1988.png"
+    },
+    {
+        id: "2",
+        pname: "庆余年",
+        price: 38,
+        pdesc: "《庆余年》是首发于起点中文网的一部架空历史小说，作者是猫腻 ",
+        imgUrl: "img/庆余年.png"
+    },
+    {
+        id: "3",
+        pname: "小王子",
+        price: 18,
+        pdesc: "《小王子》是法国作家安托万·德·圣·埃克苏佩里于1942年写成的著名儿童文学短篇小说",
+        imgUrl: "img/小王子.png"
+    }
+]
---
+++ HTML项目 网上书城 部分/products.js
@@ -0,0 +1,23 @@
+let products =[
+    {
+        id:"1",
+        pname:"毛衣",
+        price:300.1,
+        pdesc:"大码女装胖mm吊带连衣裙加外搭海边度假沙滩裙法式少女超仙两件套",
+        imgUrl:"img/通灵王.png"
+    },
+    {
+        id:"2",
+        pname:"毛衣",
+        price:369.8,
+        pdesc:"大码女装胖mm吊带连衣裙加外搭海边度假沙滩裙法式少女超仙两件套 ",
+        imgUrl:"img/通灵王2.png"
+    },
+     {
+        id:"3",
+        pname:"毛衣",
+        price:369.8,
+        pdesc:"大码女装胖mm吊带连衣裙加外搭海边度假沙滩裙法式少女超仙两件套 ",
+        imgUrl:"img/通灵王.png"
+    }
+]
